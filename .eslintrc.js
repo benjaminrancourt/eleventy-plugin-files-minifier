d7a47a8a3c5525b5e5eb2d4fa15745e627161ee1
Benjamin Rancourt <Benjamin.Rancourt@USherbrooke.ca>
2021-10-25 15:04:55 -0400

    Update dependencies
    Chores

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Extends the UdeS ESLint config
-  extends: "eslint-config-udes/node-14",
+  extends: "eslint-config-udes/node-16",
 
   // Limit ESLint to a specific project
   root: true,
